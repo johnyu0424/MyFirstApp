224f70e9d5cff2e61988a097ece5e7f9ba6a5462
John Yu <tsungyen@asu.edu>
2014-01-22 23:28:59 -0700

    Finished my first app. The app will output the message enter on the first screen to the second screen.

--- MyFirstApp/src/main/java/me/johnyu/myfirstapp/MainActivity.java
+++ MyFirstApp/src/main/java/me/johnyu/myfirstapp/MainActivity.java
@@ -1,17 +1,18 @@
 package me.johnyu.myfirstapp;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.EditText;
 
 public class MainActivity extends ActionBarActivity {
+    public final static String EXTRA_MESSAGE = "me.johnyu.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,6 +61,16 @@ public class MainActivity extends ActionBarActivity {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
         }
+    }
+
+    // Code for action after send button is pressed
+    public void sendMessage(View view)
+    {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 
 }
